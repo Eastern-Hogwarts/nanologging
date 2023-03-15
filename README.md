d4b328484a1c6cb7f8952a4709ef33f53aae9b6d
lygztq <lygztq@gmail.com>
2023-03-15 21:50:11 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 NanoLogging
 ============
+A light and header-only logging library for debug propose based on C++17.
 
+Usage
+-----
 
+```c++
+#include "nanologging\nanologging.h"
+
+int main() {
+  LOG_INFO("This is LOG_INFO with value ", 123);
+  LOG_WARN("This is LOG_WARN with value ", 0.01);
+  // LOG_ERROR("This is LOG_ERROR with value ", 'c', " and throw");
+
+  CHECK_EQ(1, 1);
+  // CHECK_EQ(1, 2);
+
+  return 0;
+}
+```
--- include/nanologging/nanologging.h
+++ include/nanologging/nanologging.h
@@ -1,42 +1,20 @@
 /*!
- * @copyright Copyright (c) 2021
- * @file nanologging.h
- * @brief defines logging macros of nanologging
+ * \copyright Copyright (c) 2021
+ * \file nanologging.h
+ * \brief defines logging macros of nanologging
  */
 
 #ifndef NANOLOGGING_H_
 #define NANOLOGGING_H_
 
-#include <stdexcept>
-#include <string>
+#include <ctime>
 #include <iostream>
 #include <sstream>
-#include <ctime>
-
-// force-inline macro
-#if defined(__GNUC__) || defined(__clang__)
-#define NANOLOGGING_ALWAYS_INLINE inline __attribute__((__always_inline__))
-#elif defined(_MSC_VER)
-#define NANOLOGGING_ALWAYS_INLINE __forceinline
-#else
-#define NANOLOGGING_ALWAYS_INLINE inline
-#endif
+#include <stdexcept>
+#include <string>
 
 namespace nanologging {
 
-/**
- * @brief exception class that will be thrown by
- *  default logger
- */
-struct Error : public std::runtime_error {
-  /**
-   * @brief Construct a new Error object
-   * @param s the error msg
-   */
-  explicit Error(const std::string &s) : std::runtime_error(s) {}
-};
-
-/** @brief Time logger */
 class DateLogger {
 public:
   DateLogger() {
@@ -47,7 +25,7 @@ public:
 #endif
   }
 
-  const char* HumanDate() {
+  const char *HumanDate() {
 #ifdef _MSC_VER
     _strtime_s(m_buffer, sizeof(m_buffer));
 #else
@@ -57,21 +35,62 @@ public:
     struct tm now;
     pnow = localtime_r(&time_value, &now);
 #else
-    pnow = localtime(&time_value);  // NOLINT(*)
+    pnow = localtime(&time_value); // NOLINT(*)
 #endif // _WIN32
-    snprintf(m_buffer, sizeof(m_buffer), "%02d:%02d:%02d",
-             pnow->tm_hour, pnow->tm_min, pnow->tm_sec);
+    snprintf(m_buffer, sizeof(m_buffer), "%02d:%02d:%02d", pnow->tm_hour,
+             pnow->tm_min, pnow->tm_sec);
 #endif // _MSC_VER
     return m_buffer;
+  }
+
+  static DateLogger &Get() {
+    thread_local DateLogger obj_;
+    return obj_;
   }
 
 private:
   char m_buffer[9];
 };
 
-
+template <typename... Messages> std::string PrintMessage(Messages &&...msgs) {
+  std::ostringstream sm;
+  (sm << ... << msgs) << std::endl;
+  return sm.str();
+}
 
 } // namespace nanologging
 
+#define MESSAGE_AND_THROW_(...)                                                \
+  {                                                                            \
+    auto msg = nanologging::PrintMessage(__VA_ARGS__);                         \
+    std::cerr << "[LOG ERROR] - "                                              \
+              << nanologging::DateLogger::Get().HumanDate()                    \
+              << " - File: " << __FILE__ << ":" << __LINE__ << ": " << msg;    \
+    throw std::runtime_error(msg);                                             \
+  }
+
+#define LOG_(level, ...)                                                       \
+  {                                                                            \
+    std::cerr << "[LOG " #level << "] - "                                      \
+              << nanologging::DateLogger::Get().HumanDate()                    \
+              << " - File: " << __FILE__ << ":" << __LINE__ << ": "            \
+              << nanologging::PrintMessage(__VA_ARGS__);                       \
+  }
+
+#define LOG_INFO(...) LOG_(INFO, __VA_ARGS__)
+#define LOG_WARN(...) LOG_(WARN, __VA_ARGS__)
+#define LOG_ERROR(...) MESSAGE_AND_THROW_(__VA_ARGS__)
+
+#define CHECK(x)                                                               \
+  if (!(x)) {                                                                  \
+    LOG_ERROR("Check failed: " #x)                                             \
+  }
+
+#define CHECK_EQ(x, y) CHECK(x == y)
+#define CHECK_NE(x, y) CHECK(x != y)
+#define CHECK_LT(x, y) CHECK(x < y)
+#define CHECK_LE(x, y) CHECK(x <= y)
+#define CHECK_GT(x, y) CHECK(x > y)
+#define CHECK_GE(x, y) CHECK(x >= y)
 
 #endif // NANOLOGGING_H_
